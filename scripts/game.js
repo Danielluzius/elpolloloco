1b9783ea620f1153ebf29ee503e11ce55eb4846a
Daniel Luzius <danielluzius@mail.de>
2025-08-26 14:20:16 +0200

    feat: add models folder with Character, Chicken, MovableObjects and World classes

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,10 @@
+let canvas;
+let ctx;
+let world = new World();
+
 function init() {
-  // Initialization code here
+  canvas = document.getElementById('canvas');
+  ctx = canvas.getContext('2d');
+
+  console.log('My character is', world.character);
 }
