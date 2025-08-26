97f78643b122d1bc60340bbc0bde39a4d552943f
Daniel Luzius <danielluzius@mail.de>
2025-08-26 16:13:38 +0200

    feat: add new classes and place objects on canvas with size adjustments

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,1 +1,10 @@
-class Chicken extends MoveableObject {}
+class Chicken extends MoveableObject {
+  height = 60;
+  width = 80;
+  y = 360;
+  constructor() {
+    super().loadImage('assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+
+    this.x = 200 + Math.random() * 500;
+  }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -1,8 +1,40 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
+  clouds = [new Cloud()];
+  backgroundObjects = [
+    new BackgroundObject('assets/img/5_background/layers/air.png', 0, 0),
+    new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 0, 0),
+    new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 0, 0),
+    new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 0, 0),
+  ];
+  canvas;
+  ctx;
+
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+    this.draw();
+  }
 
   draw() {
-    // Draw the world
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.addObjectsToMap(this.backgroundObjects);
+    this.addToMap(this.character);
+    this.addObjectsToMap(this.enemies);
+    this.addObjectsToMap(this.clouds);
+
+    requestAnimationFrame(() => this.draw());
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach((obj) => {
+      this.addToMap(obj);
+    });
+  }
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
   }
 }
