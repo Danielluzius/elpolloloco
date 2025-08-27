05b237c1b91d4fc8e6b3ba525d1cf68427b25698
Daniel Luzius <danielluzius@mail.de>
2025-08-27 13:03:59 +0200

    feat: implement Keyboard class with arrow key controls for character movement

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MoveableObject {
   height = 280;
   y = 155;
+  speed = 10;
   IMAGES_WALKING = [
     'assets/img/2_character_pepe/2_walk/W-21.png',
     'assets/img/2_character_pepe/2_walk/W-22.png',
@@ -19,11 +20,24 @@ class Character extends MoveableObject {
 
   animate() {
     setInterval(() => {
-      let i = this.currentImage % this.IMAGES_WALKING.length;
-      let path = this.IMAGES_WALKING[i];
-      this.img = this.imageCache[path];
-      this.currentImage++;
-    }, 100);
+      if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+      }
+
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+      }
+    }, 1000 / 60);
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        // Walk animation
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 50);
   }
 
   jump() {
