e270bab71d65d9aad4693d0656f648c196166832
Daniel Luzius <danielluzius@mail.de>
2025-08-29 13:45:58 +0200

    feat: add StatusBar class to reduce character life on hit

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,4 @@
-class MoveableObject {
-  x = 120;
-  y = 120;
-  img;
-  height = 150;
-  width = 100;
-  imageCache = {};
-  currentImage = 0;
+class MoveableObject extends DrawableObject {
   speed = 0.15;
   otherDirection = false;
   speedY = 0;
@@ -24,25 +17,6 @@ class MoveableObject {
 
   isAboveGround() {
     return this.y < 120;
-  }
-
-  loadImage(path) {
-    this.img = new Image();
-    this.img.src = path;
-  }
-
-  draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  }
-
-  drawFrame(ctx) {
-    if (this instanceof Character || this instanceof Chicken) {
-      ctx.beginPath();
-      ctx.lineWidth = 5;
-      ctx.strokeStyle = 'blue';
-      ctx.rect(this.x, this.y, this.width, this.height);
-      ctx.stroke();
-    }
   }
 
   isColliding(mo) {
@@ -68,14 +42,6 @@ class MoveableObject {
 
   isDead() {
     return this.energy == 0;
-  }
-
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      this.imageCache[path] = img;
-    });
   }
 
   playAnimation(images) {
--- models/world.class.js
+++ models/world.class.js
@@ -5,6 +5,7 @@ class World {
   ctx;
   keyboard;
   camera_x = 0;
+  statusBar = new StatusBar();
 
   constructor(canvas, keyboard) {
     this.canvas = canvas;
@@ -23,8 +24,8 @@ class World {
     setInterval(() => {
       this.level.enemies.forEach((enemy) => {
         if (this.character.isColliding(enemy)) {
-          // console.log('Collision with enemy, energy lost:', this.character.energy);
           this.character.hit();
+          this.statusBar.setPercentage(this.character.energy);
         }
       });
     }, 200);
@@ -34,8 +35,11 @@ class World {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.ctx.translate(this.camera_x, 0); // alle Objekte nach links verschieben
-
     this.addObjectsToMap(this.level.backgroundObjects);
+
+    this.ctx.translate(-this.camera_x, 0);
+    this.addToMap(this.statusBar);
+    this.ctx.translate(this.camera_x, 0);
 
     this.addToMap(this.character);
     this.addObjectsToMap(this.level.enemies);
