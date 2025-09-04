06e11a4b27d3676acf6771ff89ae00ebf20a74f7
Daniel Luzius <danielluzius@mail.de>
2025-09-04 15:42:10 +0200

    refactor: replaced old background pictures in canvas to new ones

--- levels/level1.js
+++ levels/level1.js
@@ -4,50 +4,27 @@ function createLevel1() {
   const coinGen = new CoinGenerator(rng, { amount: 20, startX: 700, endX: 4200, tiersY: [160, 240, 320] });
   const bottleGen = new BottleGenerator(rng, { amount: 10, startX: 700, endX: 4200, fixedY: 360 });
 
+  // Tile nature layers across the level in the order: 3_layer, cloud_layer, 2_layer, 1_layer, bird_layer
+  const segW = 719; // BackgroundObject draws at width 720; we start with -719 like before for seamless wrap
+  const xs = [-segW, 0, segW, segW * 2, segW * 3, segW * 4, segW * 5, segW * 6, segW * 7];
+  const L3 = 'assets/img/5_background/nature/3_layer.png';
+  const LCloud = 'assets/img/5_background/nature/cloud_layer.png';
+  const L2 = 'assets/img/5_background/nature/2_layer.png';
+  const L1 = 'assets/img/5_background/nature/1_layer.png';
+  const LBird = 'assets/img/5_background/nature/bird_layer.png';
+  const bg = [];
+  xs.forEach((x) => {
+    bg.push(new BackgroundObject(L3, x, 0));
+    bg.push(new BackgroundObject(LCloud, x, 0));
+    bg.push(new BackgroundObject(L2, x, 0));
+    bg.push(new BackgroundObject(L1, x, 0));
+    bg.push(new BackgroundObject(LBird, x, 0));
+  });
+
   return new Level(
     [...enemyGen.generate(), new Endboss()],
-    [new Cloud()],
-    [
-      new BackgroundObject('assets/img/5_background/layers/air.png', -719, 0),
-      new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', -719, 0),
-      new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', -719, 0),
-      new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', -719, 0),
-
-      new BackgroundObject('assets/img/5_background/layers/air.png', 0, 0),
-      new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 0, 0),
-      new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 0, 0),
-      new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 0, 0),
-      new BackgroundObject('assets/img/5_background/layers/air.png', 719, 0),
-      new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 719, 0),
-      new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 719, 0),
-      new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 719, 0),
-
-      new BackgroundObject('assets/img/5_background/layers/air.png', 719 * 2, 0),
-      new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 719 * 2, 0),
-      new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 719 * 2, 0),
-      new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 719 * 2, 0),
-      new BackgroundObject('assets/img/5_background/layers/air.png', 719 * 3, 0),
-      new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 719 * 3, 0),
-      new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 719 * 3, 0),
-      new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 719 * 3, 0),
-
-      new BackgroundObject('assets/img/5_background/layers/air.png', 719 * 4, 0),
-      new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 719 * 4, 0),
-      new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 719 * 4, 0),
-      new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 719 * 4, 0),
-      new BackgroundObject('assets/img/5_background/layers/air.png', 719 * 5, 0),
-      new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 719 * 5, 0),
-      new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 719 * 5, 0),
-      new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 719 * 5, 0),
-      new BackgroundObject('assets/img/5_background/layers/air.png', 719 * 6, 0),
-      new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 719 * 6, 0),
-      new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 719 * 6, 0),
-      new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 719 * 6, 0),
-      new BackgroundObject('assets/img/5_background/layers/air.png', 719 * 7, 0),
-      new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 719 * 7, 0),
-      new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 719 * 7, 0),
-      new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 719 * 7, 0),
-    ],
+    [], // clouds handled by cloud_layer background
+    bg,
     coinGen.generate(),
     bottleGen.generate()
   );
